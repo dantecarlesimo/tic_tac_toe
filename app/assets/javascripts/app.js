9f4a00bca0ac0542fa6ed92581ce8ddc1236143e
Dante Carlesimo <dantecarlesimo@gmail.com>
2014-06-08 20:14:27 -0700

    refactored code

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,133 +1,44 @@
 GameApp = angular.module("GameApp", [])
 
-
-
 GameApp.controller("GamesCtrl", function($scope){
   $scope.plays = {};
   $scope.count = 0;
 
-  $scope.addPlay1 = function(){
-    // $scope.plays.push($scope.newPlay);
+  $scope.addPlay = function(play){
     if ($scope.count %2 == 0){
-    $scope.play1 = "X";
+    $scope.plays[play] = "X";
     }  else {
-    $scope.play1 = "O";
+    $scope.plays[play] = "O";
     }
     $scope.count += 1;
-    $scope.plays.play1 = $scope.play1;
-    console.log($scope.count);
-    console.log($scope.play1);
-    console.log($scope.count %2)
-  };
-    $scope.addPlay2 = function(){
-    // $scope.plays.push($scope.newPlay);
-    if ($scope.count %2 == 0){
-    $scope.play2 = "X";
-    }  else {
-    $scope.play2 = "O";
-    }
-    $scope.count += 1;
-    $scope.plays.play2 = $scope.play2;
     console.log($scope.count);
     console.log($scope.plays);
-      console.log($scope.count %2)
+  };
 
-  };
-    $scope.addPlay3 = function(){
-    // $scope.plays.push($scope.newPlay);
-    if ($scope.count %2 == 0){
-    $scope.play3 = "X";
-    }  else {
-    $scope.play3 = "O";
+  $scope.score = function(){
+    if ((($scope.plays.play1 && $scope.plays.play2 && $scope.plays.play3)||
+         ($scope.plays.play4 && $scope.plays.play5 && $scope.plays.play6)||
+         ($scope.plays.play7 && $scope.plays.play8 && $scope.plays.play9)||
+         ($scope.plays.play1 && $scope.plays.play4 && $scope.plays.play7)||
+         ($scope.plays.play2 && $scope.plays.play5 && $scope.plays.play8)||
+         ($scope.plays.play3 && $scope.plays.play6 && $scope.plays.play9)||
+         ($scope.plays.play1 && $scope.plays.play5 && $scope.plays.play9)||
+         ($scope.plays.play3 && $scope.plays.play5 && $scope.plays.play7)) == "X") {
+    $scope.winner = "X";
+    } else if ((($scope.plays.play1 && $scope.plays.play2 && $scope.plays.play3)||
+         ($scope.plays.play4 && $scope.plays.play5 && $scope.plays.play6)||
+         ($scope.plays.play7 && $scope.plays.play8 && $scope.plays.play9)||
+         ($scope.plays.play1 && $scope.plays.play4 && $scope.plays.play7)||
+         ($scope.plays.play2 && $scope.plays.play5 && $scope.plays.play8)||
+         ($scope.plays.play3 && $scope.plays.play6 && $scope.plays.play9)||
+         ($scope.plays.play1 && $scope.plays.play5 && $scope.plays.play9)||
+         ($scope.plays.play3 && $scope.plays.play5 && $scope.plays.play7)) == "O") {
+    $scope.winner = "O"; 
     }
-    $scope.count += 1;
-    $scope.plays.play3 = $scope.play3;
-    console.log($scope.count);
-    console.log($scope.plays);
-        console.log($scope.count %2)
-
   };
-    $scope.addPlay4 = function(){
-    // $scope.plays.push($scope.newPlay);
-    if ($scope.count %2 === 0){
-    $scope.play4 = "X";
-    }  else {
-    $scope.play4 = "O";
-    }
-    $scope.count += 1;
-    $scope.plays.play4 = $scope.play4;
-    console.log($scope.count);
-    console.log($scope.plays);
-  };
-    $scope.addPlay5 = function(){
-    // $scope.plays.push($scope.newPlay);
-    if ($scope.count %2 === 0){
-    $scope.play5 = "X";
-    }  else {
-    $scope.play5 = "O";
-    }
-    $scope.count += 1;
-    $scope.plays.play5 = $scope.play5;
-    console.log($scope.count);
-    console.log($scope.plays);
-  };
-    $scope.addPlay6 = function(){
-    // $scope.plays.push($scope.newPlay);
-    if ($scope.count %2 === 0){
-    $scope.play6 = "X";
-    }  else {
-    $scope.play6 = "O";
-    }
-    $scope.count += 1;
-    $scope.plays.play6 = $scope.play6;
-    console.log($scope.count);
-    console.log($scope.plays);
-  };
-    $scope.addPlay7 = function(){
-    // $scope.plays.push($scope.newPlay);
-    if ($scope.count %2 === 0){
-    $scope.play7 = "X";
-    }  else {
-    $scope.play7 = "O";
-    }
-    $scope.count += 1;
-
-    $scope.plays.play7 = $scope.play7;
-    console.log($scope.count);
-    console.log($scope.plays);
-  };
-    $scope.addPlay8 = function(){
-    // $scope.plays.push($scope.newPlay);
-    if ($scope.count %2 === 0){
-    $scope.play8 = "X";
-    }  else {
-    $scope.play8 = "O";
-    }
-    $scope.count += 1;
-
-    $scope.plays.play8 = $scope.play8;
-    console.log($scope.count);
-    console.log($scope.plays);
-  };
-    $scope.addPlay9 = function(){
-    // $scope.plays.push($scope.newPlay);
-    if ($scope.count %2 == 0){
-    $scope.play9 = "X";
-    }  else {
-    $scope.play9 = "O";
-    }
-    $scope.count += 1;
-
-    $scope.plays.play9 = $scope.play9;
-    console.log($scope.count);
-    console.log($scope.plays);
-  };
-    $scope.score = function(){
-    if (($scope.plays.play1 && $scope.plays.play2 && $scope.plays.play3) == "X") {
-      $scope.winner = "X";
-    } else {
-     $scope.winner = "NONE"
-    }
+  $scope.clear = function(){
+    $scope.plays = {};
+    $scope.count = 0;
   };
 
 });
